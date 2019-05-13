f7a90bf5dc1c687177438939fef2a6f8b6b78745
Ansh <ansh.agrawal.che17@itbhu.ac.in>
2019-05-13 23:00:11 +0530

    third commit

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var validator = require("express-validator");
 var flash = require("connect-flash");
 var ejs = require("ejs");
 var passport = require("passport");
+
+// Calling the server
 container.resolve(function (users, _ ) {
     var app = SetUpExpress();
     function SetUpExpress() {
         var app = express();
         var server = http.createServer(app);
-      
         server.listen(8000, function (err, result) {
             if (err) {
                 console.log(err);
@@ -20,14 +21,14 @@ container.resolve(function (users, _ ) {
                 console.log("Server is running on the port 8000");
             }
         });
+        // Using the Function to call the dependencies
         ConfigureExpress(app);
-        
         var router = require("express-promise-router")();
         users.SetRouting(router);
         app.use(router);
     }
+    //Calling the dependencies
     function ConfigureExpress(app) {
-        
         app.use(cookieParser());
         app.use(express.static("public"));
         app.set("view engine", "ejs");
